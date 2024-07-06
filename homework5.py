5ec1a8913b3f5f77b99eced6ed962af5a6942cde
artyr231280 <artyr231280@mail.ru>
2024-07-06 14:09:09 +0500

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,9 @@
+immutable_var = 1, 2, 3, 'a', 'b','c'
+print(immutable_var)
+#immutable_var[0] = [67] # не изменяемый тип данных
+mutable_list = [1, 2, 'a', 'b']
+print(mutable_list)
+mutable_list = [1, 2, 'a', 'b'] + ['Modified']
+print(mutable_list)
+
+
